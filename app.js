8ad24cf4bad3d83c0778865ee5fee35d8b40281a
stevepetebruce <stevepetebruce@hotmail.com>
2020-06-01 12:43:35 +0100

    Add mongoose and connect to the mongo db server.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const DB_USERNAME = process.env.MONGO_DB_USERNAME;
+const DB_PASSWORD = process.env.MONGO_DB_ACCESS;
 
 const scheduleRoutes = require("./routes/schedule-routes");
 const userRoutes = require("./routes/user-routes");
@@ -28,4 +31,18 @@ app.use((error, req, res, next) => {
 	res.json({ message: error.message || "an unknown error occured" });
 });
 
-app.listen(5000);
+const connectUrl = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@schedulecluster0-pj8ef.mongodb.net/schedules?retryWrites=true&w=majority`;
+const connectConfig = {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+};
+
+mongoose
+	.connect(connectUrl, connectConfig)
+	.then(() => {
+		app.listen(5000);
+	})
+	.catch((error) => {
+		console.log(error);
+	});
