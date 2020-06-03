60235c312d163f64fb7a86f4ebe0625c59116880
stevepetebruce <stevepetebruce@hotmail.com>
2020-06-03 22:47:02 +0100

    Add new user schema and create user middlewear.

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,6 +1,8 @@
-const HttpError = require("../models/http-error");
-const { v4: uuidv4 } = require("uuid");
 const { validationResult } = require("express-validator");
+const { v4: uuidv4 } = require("uuid");
+
+const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const DUMMYUSERS = [
 	{
@@ -15,28 +17,38 @@ const getUsers = (req, res, next) => {
 	res.json({ users: DUMMYUSERS });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return next(new HttpError("Invalid inputs", 422));
 	}
 
-	const { name, email, password } = req.body;
+	const { name, email, password, schedules } = req.body;
 
-	const hasUser = DUMMYUSERS.find((u) => u.email === email);
-	if (hasUser) {
-		return next(new HttpError("Email is currently in use", 422));
+	let existingUser;
+	try {
+		existingUser = await User.findOne({ email: email });
+	} catch (error) {
+		return next(new HttpError("Failed to find user", 500));
 	}
 
-	const createdUser = {
-		id: uuidv4(),
+	if (existingUser) {
+		return next(new HttpError("User already exists. Please log in", 422));
+	}
+
+	const createdUser = new User({
 		name,
 		email,
 		password,
-	};
+		schedules,
+	});
 
-	DUMMYUSERS.push(createdUser);
-	res.status(201).json({ user: createdUser });
+	try {
+		await createdUser.save();
+	} catch (error) {
+		return next(new HttpError("Sign up failed", 500));
+	}
+	res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 const login = (req, res, next) => {
