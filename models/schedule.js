a0f889399d3c9dd64bba9ccfea88189b1fd1f9c6
stevepetebruce <stevepetebruce@hotmail.com>
2020-06-28 18:05:35 +0100

    Add time stamps to schedule schema to allow me to save created and updated dates

--- models/schedule.js
+++ models/schedule.js
@@ -2,19 +2,24 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const scheduleSchema = new Schema({
-	title: { type: String, required: true },
-	description: { type: String, required: true },
-	image: { type: String, required: true },
-	address: { type: String, required: true },
-	location: {
-		lat: { type: Number, required: true },
-		lng: { type: Number, required: true },
+const scheduleSchema = new Schema(
+	{
+		title: { type: String, required: true },
+		description: { type: String, required: true },
+		image: { type: String, required: true },
+		address: { type: String, required: true },
+		location: {
+			lat: { type: Number, required: true },
+			lng: { type: Number, required: true },
+		},
+		startDate: { type: Date, required: true },
+		daysQty: { type: Number, required: true },
+		scheduleList: { type: Array, required: true },
+		creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
 	},
-	startDate: { type: Date, required: true },
-	daysQty: { type: Number, required: true },
-	scheduleList: { type: Array, required: true },
-	creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
-});
+	{
+		timestamps: true,
+	}
+);
 
 module.exports = mongoose.model("Schedule", scheduleSchema);
